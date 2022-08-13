12e6ad51ceea5bc29e6f3a4d7b6a57f49ce6f5e9
Yannik Hangst <yanso1@posteo.de>
2022-08-13 17:47:18 +0200

    implement zustand for notes

--- src/components/Note/NoteList/NoteList.js
+++ src/components/Note/NoteList/NoteList.js
@@ -1,27 +1,9 @@
-import { useState } from "react";
+// import { useState } from "react";
+import useStore from "../../../hooks/useStore";
 import NoteItem from "../NoteItem/NoteItem";
 
 export default function NoteList() {
-  const [notes, setNotes] = useState([
-    {
-      id: 1,
-      title: "note #1",
-      body: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-      date: "XX.XX.XXXX",
-    },
-    {
-      id: 2,
-      title: "note #2",
-      body: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-      date: "XX.XX.XXXX",
-    },
-    {
-      id: 3,
-      title: "note #3",
-      body: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-      date: "XX.XX.XXXX",
-    },
-  ]);
+  const notes = useStore((state) => state.notes);
 
   return (
     <>
--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,2 +1,29 @@
 import create from "zustand";
 import { nanoid } from "nanoid";
+
+const useStore = create((set) => {
+  return {
+    notes: [
+      {
+        id: nanoid(),
+        title: "note #1",
+        body: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+        date: "XX.XX.XXXX",
+      },
+      {
+        id: nanoid(),
+        title: "note #2",
+        body: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+        date: "XX.XX.XXXX",
+      },
+      {
+        id: nanoid(),
+        title: "note #3",
+        body: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+        date: "XX.XX.XXXX",
+      },
+    ],
+  };
+});
+
+export default useStore;
