1b552f076e1d9da87b1741749fb9c3dae9226fdf
Yannik Hangst <yanso1@posteo.de>
2022-08-13 23:33:54 +0200

    add correct date

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -8,19 +8,19 @@ const useStore = create((set) => {
         id: nanoid(),
         title: "note #1",
         body: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-        date: "XX.XX.XXXX",
+        date: new Date("December 17, 1995").toISOString(),
       },
       {
         id: nanoid(),
         title: "note #2",
         body: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-        date: "XX.XX.XXXX",
+        date: new Date("December 17, 1995").toISOString(),
       },
       {
         id: nanoid(),
         title: "note #3",
         body: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-        date: "XX.XX.XXXX",
+        date: new Date("December 17, 1995").toISOString(),
       },
     ],
   };
