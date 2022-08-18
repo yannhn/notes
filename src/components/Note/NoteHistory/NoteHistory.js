0b17cdff51c988dcfdde0f4709e278a02f5736dc
Yannik Hangst <yanso1@posteo.de>
2022-08-18 11:11:00 +0200

    comment for later work

--- src/components/Note/NoteHistory/NoteHistory.js
+++ src/components/Note/NoteHistory/NoteHistory.js
@@ -8,19 +8,6 @@ export default function NoteHistory() {
   const dates = notes.map((note) => note.date);
   const uniqueDates = [...new Set(dates)];
 
-  //TODO: Should only display the input of the current week
-  //TODO: filter by current week?
-
-  //FIXME: Currently just shows the last input
-
-  //TODO: The Note should always show the note of the current week
-  //FIXME: There should be no button to input a date, it should automatically detect when the note was created
-
-  const sortedDate = uniqueDates.sort((a, b) => new Date(b) - new Date(a));
-  console.log(sortedDate);
-  const recentDate = sortedDate.slice(0, 1);
-  console.log(recentDate);
-
   return (
     <>
       {uniqueDates
