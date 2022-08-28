9da15e510a8b4f0ff072f2c1d5a57f18bf5ae4c8
Yannik Hangst <yanso1@posteo.de>
2022-08-28 23:14:51 +0200

    add delete button to note-item

--- src/components/Note/NoteItem/NoteItem.js
+++ src/components/Note/NoteItem/NoteItem.js
@@ -1,11 +1,14 @@
 import NoteCard from "../NoteCard/NoteCard";
 import { StyledNoteTitle } from "./Styled";
 
-export default function NoteItem({ title, body }) {
+export default function NoteItem({ title, body, deleteNote }) {
   return (
     <NoteCard>
       <StyledNoteTitle>{title}</StyledNoteTitle>
       <p>{body}</p>
+      <button type="button" onClick={deleteNote}>
+        Delete Note
+      </button>
     </NoteCard>
   );
 }
--- src/components/Note/NoteList/NoteList.js
+++ src/components/Note/NoteList/NoteList.js
@@ -5,17 +5,22 @@ import NoteItem from "../NoteItem/NoteItem";
 
 export default function NoteList() {
   const notes = useStore((state) => state.notes);
+  const deleteNote = useStore((state) => state.deleteNote);
 
   const today = moment().format("WW");
 
   const filteredItems = notes.filter((notes) => notes.date === today);
-  console.log(filteredItems);
 
   return (
     <>
       <h2>Week: {today} of 2022</h2>
       {filteredItems.map((note) => (
-        <NoteItem key={note.id} title={note.title} body={note.body} />
+        <NoteItem
+          key={note.id}
+          title={note.title}
+          body={note.body}
+          deleteNote={() => deleteNote(note.id)}
+        />
       ))}
     </>
   );
