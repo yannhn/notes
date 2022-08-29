e9ec38660e72123a5c2fd8d0161167358c111af6
Yannik Hangst <yanso1@posteo.de>
2022-08-29 16:55:15 +0200

    implement delete functionality

--- src/components/UI/DeleteNoteMessage/DeleteNoteMessage.js
+++ src/components/UI/DeleteNoteMessage/DeleteNoteMessage.js
@@ -1,3 +1,13 @@
+import { useStore } from "zustand";
+
 export default function DeleteNoteMessage() {
-  return <></>;
+  //   const deleteNote = useStore((state) => state.deleteNote);
+
+  return (
+    <>
+      <p>Are you sure you want to delete that note?</p>
+      <button type="button">Cancel</button>
+      <button type="button">Delete Note</button>
+    </>
+  );
 }
