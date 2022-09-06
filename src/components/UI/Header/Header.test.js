9c59ca16bae863665a95290b032aa9bdfa4228ed
Yannik Hangst <yanso1@posteo.de>
2022-09-06 22:34:31 +0200

    fix header Test

--- src/components/UI/Header/Header.test.js
+++ src/components/UI/Header/Header.test.js
@@ -2,12 +2,20 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
 import Header from "./Header";
+import { StyledTitle } from "./Styled";
 
 describe("Header Component", () => {
-  it("should render Header text", () => {
-    render(<Header />);
-
-    const title = screen.getByText("Notes");
+  it("should render Title text", () => {
+    const text = "Notes";
+    render(<StyledTitle>{text}</StyledTitle>);
+    const title = screen.getByText(text, { exact: false });
     expect(title).toBeInTheDocument();
+  });
+});
+
+describe("Header links", () => {
+  it("should link to other pages", () => {
+    const handleClick = jest.fn();
+    const text = "";
   });
 });
